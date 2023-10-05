a275ec4358e031d73396d812b01b4fa82d730a1e
kimhj7734 <kimhj7734@gmail.com>
2023-10-05 18:41:50 +0900

    update
    유니티 버전 업글 및 프로젝트 세팅

---
+++ firstTopDown/Assets/scripts/InGameView Button/Button UI/commonUIBtn.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class commonUIBtn : MonoBehaviour
+{
+    void Start() {
+        
+    }
+
+    void Update() {
+        
+    }
+}
