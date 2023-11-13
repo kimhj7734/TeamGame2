6fbb43e060c968203f90ee4a1d04a177b9a26aad
kimhj7734 <kimhj7734@gmail.com>
2023-11-13 14:07:25 +0900

    23.11.13 / 환경설정 및 이미지파일 정리
    유니티 환경설정, 이미지, 스크립트 파일 정리 (새 폴더 생성 > 파일이동)

---
+++ firstTopDown/Assets/scripts/InGameView Button/MonsterScripts/RandomPrefabSpawner.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPrefabSpawner : MonoBehaviour
+{
+    public GameObject[] prefabList;
+    public int numberOfPrefabsToSpawn = 100; // 생성할 프리팹의 수
+    public float spawnRadius = 2.0f; // 생성 반경
+    public float spawnInterval = 1.0f; // 생성 간격 (초)
+
+    private int spawnedCount = 0; // 이미 생성한 프리팹 수
+    public float moveSpeed =  0.0005f; // 이동 속도
+    private float lastSpawnTime = 0;
+
+    private bool canSpawn = true;
+
+    public int damageAmount = 10;
+
+    
+    void Start()
+    {
+        lastSpawnTime = Time.time; // 시작 시간 초기화
+    }
+
+    void Update()
+    {
+        if (spawnedCount < numberOfPrefabsToSpawn && Time.time - lastSpawnTime >= spawnInterval)
+        {
+            SpawnPrefab();
+            lastSpawnTime = Time.time;
+        }
+    }
+
+    void SpawnPrefab()
+    {
+        Vector2 randomCircle = Random.insideUnitCircle.normalized * spawnRadius;
+        float randomY = Random.Range(-2.5f, 2.5f); // 여기서 minY와 maxY는 y 좌표의 범위
+        float randomX = Random.Range(-2, 2); // 여기서 minZ와 maxZ는 z 좌표의 범위
+        Vector3 spawnPosition = transform.position + new Vector3(randomX * spawnRadius, randomY * spawnRadius, 0);
+        int randomIndex = Random.Range(0, prefabList.Length);
+        // Debug.Log("Spawn Position: " + spawnPosition);
+        // Debug.Log("Random Index: " + randomIndex);
+        Instantiate(prefabList[randomIndex], spawnPosition, Quaternion.identity);
+        
+        if (canSpawn)
+        {
+        canSpawn = false;
+        // 프리팹 생성
+        GameObject newPrefab = Instantiate(prefabList[randomIndex], spawnPosition, Quaternion.identity);
+        StartCoroutine(MovePrefabToZero(newPrefab.transform));
+        spawnedCount++;
+        }
+
+        spawnedCount++;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("CharacterCollider"))
+        {
+            // 정중앙 캐릭터와 충돌한 경우
+            // 데미지를 입히고 정중앙 플레이어를 파괴하지 않음
+            Health health = other.GetComponent<Health>();
+            if (health != null)
+            {
+                health.TakeDamage(damageAmount);
+            }
+        }
+        else if (other.CompareTag("Prefab"))
+        {
+            // 프리팹과 충돌한 경우
+            // 데미지를 입히고 프리팹을 제거
+            Health prefabHealth = other.GetComponent<Health>();
+            if (prefabHealth != null)
+            {
+                prefabHealth.TakeDamage(damageAmount);
+            }
+            Destroy(other.gameObject);
+        }
+    }
+
+        // 프리팹을 목표 위치로 천천히 이동시키는 코루틴
+    IEnumerator MovePrefabToZero(Transform prefabTransform)
+    {
+        Vector3 targetPosition = Vector3.zero;
+        float elapsedTime = 0f;
+
+        while (prefabTransform != null)
+        {
+            prefabTransform.position = Vector3.Lerp(prefabTransform.position, targetPosition, elapsedTime);
+            elapsedTime += Time.deltaTime * moveSpeed;
+            yield return null;
+        }
+    }
+}
