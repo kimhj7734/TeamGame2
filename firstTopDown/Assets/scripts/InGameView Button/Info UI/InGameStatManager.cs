1db3850d5c954072bdd0553cf33f82342a8bebb8
kimhj7734 <kimhj7734@gmail.com>
2023-12-05 14:45:41 +0900

    23.12.05 / 이미지 파일
    이미지 파일 슬라이스, 플레이어 콜라이더 Radius 기존 30 > 20 수정 , Prefab Create Zone Radius 기존 1 > 0.6 수정

--- firstTopDown/Assets/scripts/InGameView Button/Info UI/InGameStatManager.cs
+++ firstTopDown/Assets/scripts/InGameView Button/Info UI/InGameStatManager.cs
@@ -16,29 +16,21 @@ public class InGameStatManager : MonoBehaviour
         AtkText = GameObject.Find("UI Canvas").transform.Find("UI Panel 1").transform.Find("AtkObject").GetComponentInChildren<TMP_Text>();
         if(AtkText == null) {
             Debug.Log("not found AtkText");
-        } else {
-            Debug.Log(AtkText);
         }
 
         AtkSpdText = GameObject.Find("UI Canvas").transform.Find("UI Panel 1").transform.Find("AtkSpdObject").GetComponentInChildren<TMP_Text>();
         if (AtkSpdText == null) {
             Debug.Log("not found AtkText");
-        } else {
-            Debug.Log(AtkSpdText);
         }
 
         HealthText = GameObject.Find("UI Canvas").transform.Find("UI Panel 1").transform.Find("HealthObject").GetComponentInChildren<TMP_Text>();
         if (HealthText == null) {
             Debug.Log("not found AtkText");
-        } else {
-            Debug.Log(HealthText);
         }
 
         CriticalText = GameObject.Find("UI Canvas").transform.Find("UI Panel 1").transform.Find("CriticalObject").GetComponentInChildren<TMP_Text>();
         if (CriticalText == null) {
             Debug.Log("not found AtkText");
-        } else {
-            Debug.Log(CriticalText);
         }
     }
 
